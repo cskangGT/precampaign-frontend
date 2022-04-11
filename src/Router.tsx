d10e0983923f167e4bbb32ddfede6099774fe3c8
Seonghun Kang <gceohoony@gmail.com>
2022-04-11 11:10:04 +0900

    filter, sort, fetching with token, useNavigate : Done

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AcceptedApplicantsList from './pages/AcceptedApplicantsList/AcceptedApplicantsList';
 import Login from './pages/Login/Login';
+import CampaignList from './pages/CampaignList/CampaignList';
 
 function Router() {
   return (
@@ -9,6 +10,7 @@ function Router() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/accepted-applicants-list" element={<AcceptedApplicantsList />} />
+        <Route path="/campaigns" element={<CampaignList />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/types.tsx
@@ -0,0 +1,11 @@
+export interface CampaignCardInfo {
+  id: number;
+  thumbnail_url: string;
+  title: string;
+  evaluation_start_date: string;
+}
+
+export interface QueryInfo {
+  query_for_status: string;
+  query_for_sort: string;
+}
