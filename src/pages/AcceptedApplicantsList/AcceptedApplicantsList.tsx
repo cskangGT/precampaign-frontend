9ab1e034d39cda41b92e54514c898cecc6976713
cos <junzerokim@gmail.com>
2022-04-12 13:52:43 +0900

    Add: feature/AcceptedApplicantsList

--- src/pages/AcceptedApplicantsList/AcceptedApplicantsList.tsx
+++ src/pages/AcceptedApplicantsList/AcceptedApplicantsList.tsx
@@ -2,5 +2,11 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 export default function AcceptedApplicantsList() {
-  return <div>hello</div>;
+  return <Container></Container>;
 }
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: lightgray;
+`;
