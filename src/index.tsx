d0062de0b92ebe8c585cc0df839f0989d9cfd537
cos <junzerokim@gmail.com>
2022-04-16 17:40:04 +0900

    Add: Global

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './Router';
 
+import GlobalStyle from './styles/GlobalStyle';
+import { ThemeProvider } from '@emotion/react';
+import theme from './styles/theme';
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<Router />, rootElement);
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Router />
+    </ThemeProvider>
+  </>,
+  rootElement,
+);
