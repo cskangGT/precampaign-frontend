402453950c1f38d1422ff6d31c761dc51fe1c8a5
cos <junzerokim@gmail.com>
2022-04-18 18:32:00 +0900

    UI/ApplicantsList

--- src/pages/Applicantslist/RateModal/ApplicantPics/ApplicantPics.tsx
+++ src/pages/Applicantslist/RateModal/ApplicantPics/ApplicantPics.tsx
@@ -14,7 +14,7 @@ export default function ApplicantPics({ thumbnail }: ApplicantPicsProps) {
 
 const Container = styled.div`
   width: 450px;
-  height: 450p dx;
+  height: 450px;
   display: flex;
   flex-direction: column;
   align-items: center;
